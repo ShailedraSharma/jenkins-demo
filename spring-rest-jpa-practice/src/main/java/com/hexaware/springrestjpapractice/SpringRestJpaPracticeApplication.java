57e1479f518a6eaefced5b2993f56f7f88cf2b68
--replace-all <shailendra.761979@gmail.com>
2024-02-21 10:56:07 +0530

    modified the file

--- spring-rest-jpa-practice/src/main/java/com/hexaware/springrestjpapractice/SpringRestJpaPracticeApplication.java
+++ spring-rest-jpa-practice/src/main/java/com/hexaware/springrestjpapractice/SpringRestJpaPracticeApplication.java
@@ -8,7 +8,7 @@ public class SpringRestJpaPracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestJpaPracticeApplication.class, args);
-		System.out.println("some new code")
+		System.out.println("some new code");
 	}
 
 }
