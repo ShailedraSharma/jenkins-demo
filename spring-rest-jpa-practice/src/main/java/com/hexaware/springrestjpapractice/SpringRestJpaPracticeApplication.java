a3e082a34caa25c17892ecdeb4d9b853d04f7704
--replace-all <shailendra.761979@gmail.com>
2024-02-21 10:53:44 +0530

    added some demo files

--- spring-rest-jpa-practice/src/main/java/com/hexaware/springrestjpapractice/SpringRestJpaPracticeApplication.java
+++ spring-rest-jpa-practice/src/main/java/com/hexaware/springrestjpapractice/SpringRestJpaPracticeApplication.java
@@ -8,6 +8,7 @@ public class SpringRestJpaPracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestJpaPracticeApplication.class, args);
+		System.out.println("some new code")
 	}
 
 }
